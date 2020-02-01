35bf0720bb429c8230d2c84f97e9642b676db634
JorgeGGeasy <60537138+JorgeGGeasy@users.noreply.github.com>
2020-02-01 20:47:01 +0100

    Add files via upload
    Ya funciona el indice random (creo)

--- Assets/Scripts/Test_IndiceRand.cs
+++ Assets/Scripts/Test_IndiceRand.cs
@@ -10,74 +10,103 @@ public class Test_IndiceRand : MonoBehaviour
     public int preguntasFalladas = 0;
     public int categorias = 5;
     public int preguntas = 50;
-    public int cont = 0;
+    public int categoria = 0;
     public int[] ListaNegra = new int[50];
     public int contLista = 0;
+    public int size = 0;
 
-
-    public void comprobacion(int index)
-    {
-        for(int i=0; i < 50; i++)
-        {
-
-            if(ListaNegra[i] == index){
-
-                calcularIndice();
-            
-            }
-        }
-        ListaNegra[contLista] = index;
-        contLista++;
-    }
 
     public void contadordeAcertadas()
     {
         preguntasAcertadas++;
     }
 
+
+
     public void contadordeFalladas()
     {
         preguntasFalladas++;
     }
+    // Si quieres un indice pideselo a esta funcion
+    public int generarIndiceComprobado()
+    {
+
+        //Defino un index
+        int index;
+
+        do
+        {
+            //El bucle magico, si da true se devuelve, si da false se busca otro numero
+            index = calcularIndice();
+
+        } while (!comprobacion(index));
+
+        return index;
+
+    }
+
+    public bool comprobacion(int index)
+    {
+
+       for (int i=0; i<50; i++)
+        {   //Se comprueban las preguntas usadas con la nueva pregunta
+            if(ListaNegra[i]==index)
+            {
+                return false;
+            }           
+        }
+        //Se guarda la nueva pregunta en la lista Negra
+        ListaNegra[size] = index;
+        size++;
+        return true;
+
+    }
+
+
 
     public int calcularIndice()
     {
 
-        int random = Random.Range(0, 49);
-
-        for (int i = 0; i < 5; i++)
+        //Se busca un numero random desde el 0 hasta el 49
+        int index = Random.Range(0, 49);
+        
+        for(int i=0; i<5; i++)
         {
 
-            random = random - 10;
+            //Comprobacion para que no pasen numeros negativos
+            if(index-10<=0) {
+                index = index + 10;
+                break;
+            }
+            //Aqui se define la categoria a la que pertenece la pregunta
+            index = index - 10;
+            categoria++;
+            //El limite se encuentra cuando indice va a entrar en los numeros negativos
+            if (index - 10 <= 0){
 
-            if (random >= 0)
-            {
-                cont++;
 
-                if (random - 10 < 0)
-                {
-                    if (random >= 5)
-                    {
-                        random = random - 5;
-                        break;
-                    }
-                    break;
-                }
+                //algoritmo de randomizacion
+                index = index + 10 * categoria + preguntasAcertadas / 10 + preguntasFalladas / 3;
+                break;
             }
         }
 
 
-
-        int index = cont * 10 + random + preguntasAcertadas / 10 + preguntasFalladas / 3;
-
+        //prevencion de valores mayores o iguales a las preguntas totales
         if (index >= 50)
         {
+
             index = Random.Range(0, 49);
+
+        }
+        //prevencion de valores negativos
+        else if (index<0)
+        {
+
+            index = Random.Range(0,49);
+
         }
 
-        comprobacion(index);
-
         return index;
-
     }
 }
